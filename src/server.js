cd412c39531b8fb51537a01327c514d383b89890
ranggadya <ranggadya2530@gmail.com>
2025-08-15 01:14:36 +0700

    Update Validator

--- src/server.js
+++ src/server.js
@@ -1,18 +1,39 @@
-const { Hapi } = require('@hapi/hapi');
+const Hapi = require('@hapi/hapi');
+const AlbumsService = require('./services/postgress/AlbumsService');
+const SongsService = require('./services/postgress/SongsService');
+const albums = require('./api/music/albums');
+const songs = require('./api/music/songs');
 
 const init = async () => {
+    const albumsService = new AlbumsService();
+    const songsService = new SongsService();
+
     const server = Hapi.server({
-        port: 3000,
+        port: 5000,
         host: 'localhost',
         routes: {
             cors: {
-                orgigin: ['*'],
+                origin: ['*'],
             }
         }
     });
 
+    await server.register([
+        {
+            plugin: albums,
+            options: {
+                service: albumsService,
+            },
+        },
+        {
+            plugin: songs,
+            options: {
+                service: songsService,
+            },
+        },
+    ]);
     await server.start();
-    console.log(`Server sedang berjalan pada ${server.info.url}`);
+    console.log(`Server sedang berjalan pada ${server.info.uri}`);
 }
 
 init();
